56cbdf6511e194828042d933c6d50425adafbd9a
mquade <mail@technologiewerk-qua.de>
2016-09-29 18:31:21 +0200

    sdf

--- db.php
+++ db.php
@@ -1,4 +1,21 @@
 <?php
 
+try {
+	$pdo = new PDO("mysql:host=agriot-dev-1.cjzfegknb3zi.eu-central-1.rds.amazonaws.com;dbname=agriot_productive", 'dbroot', '');
+} catch (PDOException $e) {
+	echo 'Error: ' . htmlspecialchars($e->getMessage());
+	exit();
+	
+}
 
+$sql = "SELECT * FROM agriot_productive.users";
+$result = $db->query($sql);
+echo "<ul>";
+	foreach ($result as $row){
+		echo	"<li>" . htmlspecialchars($row["id"]) .
+				": " . htmlspecialchars($row["firstName"]) . "</li>";
+		
+		
+	}
+	echo "</ul>";
 ?>
