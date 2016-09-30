0e7ede152efadb5a41210a609852c46ec41fb89c
mquade <mail@technologiewerk-qua.de>
2016-09-30 10:29:49 +0200

    DB Connect ausgelagert

--- dbConnect.php
+++ dbConnect.php
@@ -1,6 +1,13 @@
 <?php
+$server = 'mysql:dbname=datenbank; host=localhost; port=3306';
+$user = 'user';
+$password = 'pw';
+$options = array (
+		PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8' 
+);
+
 try {
-	$db = new PDO ( '', '', '' );
+	$db = new PDO ( $server, $user, $password, $options );
 } catch ( PDOException $e ) {
 	echo 'Error: ' . htmlspecialchars ( $e->getMessage () );
 	exit ();
